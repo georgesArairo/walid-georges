9e75a4565dbc6faf701b88a0d04f46567a7367b4
georgesArairo <georgesaj5t@gmail.com>
2024-01-22 16:58:55 +0200

    delete my old code

--- db.js
+++ db.js
@@ -13,21 +13,3 @@ const connectToDb = async () => {
 };
 
 module.exports = connectToDb;
-
-//old code with mongo client
-// const {MongoClient} = require('mongodb')
-
-// let dbConnection ;
-// module.exports = {
-//     connectToDb : (cb)=> {
-//         MongoClient.connect('mongodb://localhost:27017/myDb')
-//         .then((client)=>{
-//             dbConnection = client.db
-//             return cb()
-//         }) .catch((err)=>{
-//             console.log(err)
-//             return cb(err)
-//         })
-//     } , 
-//     getDb : () => dbConnection
-// }
